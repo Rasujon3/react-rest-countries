43aa9ec347ee192aacb6797b2745fb6bb6e7c7a7
Rasujon3 <rasujon3@gmail.com>
2022-03-17 00:08:52 +0600

    46-5 Create child component and pass data via props

--- src/components/Countries/Countries.js
+++ src/components/Countries/Countries.js
@@ -12,8 +12,13 @@ const Countries = () => {
   return (
     <div>
       <h1>Hello from Countries: {countries.length}</h1>
+      {/* {countries.map((country) => console.log(country))} */}
       {countries.map((country) => (
-        <Country name={country.name.common}></Country>
+        <Country
+          name={country.name.common}
+          population={country.population}
+          area={country.area}
+        ></Country>
       ))}
     </div>
   );
--- src/components/Country/Country.js
+++ src/components/Country/Country.js
@@ -1,10 +1,16 @@
 import React from "react";
+import "./Country.css";
 
-const Country = ({ name }) => {
+const Country = ({ name, population, area }) => {
   //   console.log(props);
+  console.log(area);
   return (
-    <div>
+    <div className="country">
       <h1>Name : {name}</h1>
+      <p>Population: {population}</p>
+      <p>
+        <small>Area: {area}</small>
+      </p>
     </div>
   );
 };
