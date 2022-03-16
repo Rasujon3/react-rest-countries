3b188ac20101b1391adfbfb17a6fae9455fa0abe
Rasujon3 <rasujon3@gmail.com>
2022-03-16 23:05:35 +0600

    46-3 React Extension Pack and component with css file

--- src/App.js
+++ src/App.js
@@ -1,41 +1,15 @@
 import { useEffect, useState } from "react";
 import "./App.css";
+import Countries from "./components/Countries/Countries";
+import Header from "./components/Header/Header";
+import Person from "./components/Person/Person";
 
 function App() {
   return (
     <div className="App">
-      <LoadCountries></LoadCountries>
-    </div>
-  );
-}
-
-function LoadCountries() {
-  const [countries, setCountries] = useState([]);
-  useEffect(() => {
-    const url = `https://restcountries.com/v3.1/all`;
-    fetch(url)
-      .then((res) => res.json())
-      .then((data) => setCountries(data));
-  }, []);
-  return (
-    <div>
-      <h1>Visiting Every country of the world</h1>
-      <h3>Available Countries: {countries.length}</h3>
-      {countries.map((country) => (
-        <Country
-          name={country.name.common}
-          population={country.population}
-        ></Country>
-      ))}
-    </div>
-  );
-}
-
-function Country(props) {
-  return (
-    <div>
-      <h2>Name: {props.name}</h2>
-      <h4>Population: {props.population}</h4>
+      <Header></Header>
+      <Countries></Countries>
+      <Person></Person>
     </div>
   );
 }
